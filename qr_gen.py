1b7be523d38c065f09f9a3224f3f78932d3937df
Stefan Iancu <iancustefan28@yahoo.com>
2023-08-21 18:01:17 +0300

    2nd try

--- qr_gen.py
+++ qr_gen.py
@@ -11,6 +11,7 @@ qr = qrcode.QRCode(version=1, box_size=10, border=5)
 # qr.add_data(data)
 # qr.make(fit=True)
 
+
 # create an image from the qr
 # image = qr.make_image(fill="black", back_color="white")
 
