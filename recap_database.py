e420275e2d03c25df0ce9883c056d6490811ceee
Stefan Iancu <iancustefan28@yahoo.com>
2023-08-23 13:23:59 +0300

    jenkins

---
+++ recap_database.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+DB_PATH = r"C:\Users\User\Desktop\jenkins\random_database.db"
+
+connection = sqlite3.connect(DB_PATH)
+cursor = connection.cursor()
+
+def get_data():
+    """Gets data from database."""
+    data = cursor.execute(
+        """SELECT * FROM jenkins""", ())
+    data = cursor.fetchall()
+    for tpl in data:
+        name, age, occupation = tpl
+        print(f"{name} is {age} years old and his occupation is {occupation}.")
+
+def insert_data():
+    name = input("Name: ")
+    age = input("Age :")
+    occupation = input("Occupation: ")
+    cursor.execute(
+        """INSERT INTO jenkins (name, age, occupation) VALUES (?, ?, ?)""", (name, age, occupation)
+    )
+    connection.commit()
+
+
+get_data()
